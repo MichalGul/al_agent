80eacd4e6e9b7911b6e4b9792e1835f1729e4c68
MicGul <mwarioxin@gmail.com>
2025-07-10 17:26:47 +0200

    Added prompt as command line argument

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 from google import genai
 from google.genai.types import GenerateContentResponse
-
+import sys
 
 load_dotenv()
 API_KEY = os.environ.get("GEMINI_API_KEY")
@@ -10,12 +10,16 @@ client = genai.Client(api_key=API_KEY)
 
 def main():
     print("Hello from al-agent!")
+
+    if len(sys.argv) < 2:
+        print("Prompt not provided in arguments. Exiting...")
+        exit(1)
+
     model_response: GenerateContentResponse = client.models.generate_content( 
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=sys.argv[1]
     )
     print(f"Model response:\n {model_response.text}")
-
     print(f"Model metadata: \n Prompt tokens: {model_response.usage_metadata.prompt_token_count} \n Response tokens: {model_response.usage_metadata.candidates_token_count} \n")
     
 
