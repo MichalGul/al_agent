94680cc7f2b9aab1b7492cab61d4a261232f2015
MicGul <mwarioxin@gmail.com>
2025-07-10 17:17:56 +0200

    Created basic genai client and added hard coded request to gemini flash model

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai.types import GenerateContentResponse
+
+
+load_dotenv()
+API_KEY = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=API_KEY)
+
 def main():
     print("Hello from al-agent!")
+    model_response: GenerateContentResponse = client.models.generate_content( 
+        model="gemini-2.0-flash-001",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    print(f"Model response:\n {model_response.text}")
 
+    print(f"Model metadata: \n Prompt tokens: {model_response.usage_metadata.prompt_token_count} \n Response tokens: {model_response.usage_metadata.candidates_token_count} \n")
+    
 
 if __name__ == "__main__":
     main()
